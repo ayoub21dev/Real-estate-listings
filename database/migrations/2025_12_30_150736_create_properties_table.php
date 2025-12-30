a792605c9a9cb67d3fe2c6f70e4467f4411840b7
Adnane Kes <tangerinoh40@gmail.com>
2025-12-30 15:42:13 +0000

    create properties table

---
+++ database/migrations/2025_12_30_150736_create_properties_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->decimal('price', 12, 2);
+            $table->string('location');
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->integer('surface');
+            $table->string('listing_type');
+            $table->enum('status', ['pending', 'approved', 'rejected', 'draft'])
+          ->default('pending');
+            $table->timestamps();
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
